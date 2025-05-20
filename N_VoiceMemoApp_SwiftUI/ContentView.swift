958c908b5c8d81e8954c441806cabeee81184288
Riu <wodnd0418@naver.com>
2025-05-20 16:31:53 +0900

    Initial Commit

---
+++ N_VoiceMemoApp_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  N_VoiceMemoApp_SwiftUI
+//
+//  Created by wodnd on 5/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ N_VoiceMemoApp_SwiftUI/N_VoiceMemoApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  N_VoiceMemoApp_SwiftUIApp.swift
+//  N_VoiceMemoApp_SwiftUI
+//
+//  Created by wodnd on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct N_VoiceMemoApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
